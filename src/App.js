2d97d8d4b013ac8bf4495309dd6d55eabf0f8888
nguyễn duy tài <underwaterhp93@gmail.com>
2020-02-17 00:16:25 +0700

    creating new work

--- src/App.js
+++ src/App.js
@@ -1,26 +1,116 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import TaskList from "./components/TaskList";
+import TaskForm from "./components/TaskForm";
+import Control from "./components/TaskControl";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [], // id unique, name, status
+      isDisplayForm: true
+    }
+  }
+
+  componentDidMount() {
+    if (localStorage && localStorage.getItem('tasks')) {
+      var tasks = JSON.parse(localStorage.getItem('tasks'));
+      this.setState({
+        tasks: tasks
+      })
+    }
+  }
+
+  onGenerateData = () => {
+    var tasks = [
+      {
+        id: this.generateID(),
+        name: 'Hoc lap trinh',
+        status: true
+      },
+      {
+        id: this.generateID(),
+        name: 'Choi game',
+        status: false
+      },
+      {
+        id: this.generateID(),
+        name: 'Sleep',
+        status: true
+      }
+    ]
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+
+  s4() {
+    return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+  }
+
+  generateID() {
+    return this.s4() + this.s4() + '-' + this.s4() + '-' + this.s4() + '-' + this.s4() + '-' + this.s4()
+      + this.s4() + this.s4();
+  }
+
+  onToggleForm = () => {
+    this.setState({
+      isDisplayForm: !this.state.isDisplayForm
+    })
+  }
+
+  onCloseForm = () => {
+    this.setState({
+      isDisplayForm: false
+    })
+  }
+
+  onSubmit = (data) => {
+    var { tasks } = this.state;
+    var task = {
+      id: this.generateID(),
+      name: data.name,
+      status: data.status
+    }
+    tasks.push(task);
+    this.setState({
+      tasks: tasks
+    })
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+     
+  }
+
+  render() {
+    var { tasks, isDisplayForm } = this.state; // var tasks =this.state.tasks
+    var elmTaskForm = isDisplayForm ? <TaskForm
+      onSubmit={this.onSubmit}
+      onCloseForm={this.onCloseForm} /> : '';
+    return (
+      <div className="container">
+        <div className="text-center">
+          <h1>Quản Lý Công Việc</h1>
+          <hr />
+        </div>
+        <div className="row">
+          <div className={isDisplayForm ? 'col-xs-4 col-sm-4 col-md-4 col-lg-4' : ''}>
+            {elmTaskForm}
+          </div>
+          <div className={isDisplayForm ? 'col-xs-8 col-sm-8 col-md-8 col-lg-8' : 'col-xs-12 col-sm-12 col-md-12 col-lg-12'}>
+            <button type="button" className="btn btn-primary" onClick={this.onToggleForm}>
+              <span className="fa fa-plus mr-5" />
+              Thêm Công Việc
+            </button>
+            <button type="button" className="btn btn-danger ml-5"
+              onClick={this.onGenerateData}>
+              Generate data
+            </button>
+            <Control />
+            <TaskList tasks={tasks} />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default App
